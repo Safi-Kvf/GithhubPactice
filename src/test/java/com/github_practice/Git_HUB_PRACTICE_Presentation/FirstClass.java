b2884cc1873ce34a0300d2d47938d9f91f90c41e
Safi-Kvf <safi.khan@kualitatem.com>
2020-07-20 14:20:29 +0500

    "first class commit

---
+++ src/test/java/com/github_practice/Git_HUB_PRACTICE_Presentation/FirstClass.java
@@ -0,0 +1,13 @@
+package com.github_practice.Git_HUB_PRACTICE_Presentation;
+
+public class FirstClass {
+	
+	public static void test() {
+		
+		System.out.println("Hello world this is SAFI");
+		
+	}
+	
+	
+
+}
