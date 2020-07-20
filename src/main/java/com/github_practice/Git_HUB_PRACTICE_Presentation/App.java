41778c4f67e0f02c5ca2432d4d55ead71f0e1d17
Safi-Kvf <safi.khan@kualitatem.com>
2020-07-20 14:12:56 +0500

    first commit of new project

---
+++ src/main/java/com/github_practice/Git_HUB_PRACTICE_Presentation/App.java
@@ -0,0 +1,13 @@
+package com.github_practice.Git_HUB_PRACTICE_Presentation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
